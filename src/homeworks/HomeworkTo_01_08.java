f2e463d00a6dd71c585aa0487c85e15a14b9187f
Viktorija <viktorija.pizica@gmail.com>
2022-08-02 11:09:46 +0300

    Christmas tree

--- src/homeworks/HomeworkTo_01_08.java
+++ src/homeworks/HomeworkTo_01_08.java
@@ -33,5 +33,33 @@ public class HomeworkTo_01_08 {
 
         }
 
+        /*
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter tree size");
+        int size  = scanner.nextInt();
+
+        //Each level
+        for(int i = 0; i < size; i++){
+            //Spaces
+            for(int j = 0; j < size - 1 - i; j++){
+                System.out.print(" ");
+            }
+            //Stars
+            for(int j = 0; j < i * 2 + 1; j++){
+                System.out.print("*");
+            }
+            //Move to next line
+            System.out.println();
+        }
+
+        //Base level
+        for(int i = 0; i < size - 1; i++){
+            System.out.print(" ");
+        }
+
+        System.out.println("#");
+        */
+
     }
 }
