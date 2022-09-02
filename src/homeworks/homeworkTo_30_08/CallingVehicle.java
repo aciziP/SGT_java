befccb632ce830d96d4d9c95b1464080f5aebbe7
Viktorija <viktorija.pizica@gmail.com>
2022-09-02 13:49:01 +0300

    31.08. Lecture

--- src/homeworks/homeworkTo_30_08/CallingVehicle.java
+++ src/homeworks/homeworkTo_30_08/CallingVehicle.java
@@ -2,10 +2,10 @@ package homeworks.homeworkTo_30_08;
 
 public class CallingVehicle {
     public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle(45.5f, 5.5f, 5);
+        Vehicle vehicle = new Vehicle(55.5f, 5.5f, 5);
         System.out.println(vehicle.maxDistance());
 
-        Car car = new Car(70.0f, 7.0f, 1, true);
+        Car car = new Car(36.1f, 8.6f, 3, true);
         System.out.println(car.maxDistance());
     }
 }
